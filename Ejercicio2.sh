278e50863aabb9deadcef76e9585f10a0fa744be
Angel Ordoñez <a22orsean@iesgrancapitan.org>
2023-11-09 10:17:56 +0100

    Ejercicio Argumentos

---
+++ Ejercicio2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: 
+# Descripción: Mensaje bienvenida al usuario
+# Autor: Angel Ordoñez Serrano
+# Fecha: 9/11/2023
+
+echo $* 
+echo $#
+echo 'Has pasado' $# argumentos
+echo 'tu nombre es '$1 ' y tu apellido es ' $2
